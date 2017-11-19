3371df5111ab66099318a20cc01e5e4c7a26d3f7
Vinay Kumar Nadiminti <vnaykumar008@gmail.com>
2017-11-19 20:19:19 +0530

    Updating Program file

---
+++ PlayerPassCount/PlayerPassCount.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayerPassCount
+{
+    public class PlayerPassCount
+    {
+        public static int PassCount(int playersCount, int maxPasses, int shiftLeftOrRightCount)
+        {
+            int passCount = 0;
+            int i = 0;
+            int[] playerPassCount = new int[playersCount];
+
+            playerPassCount[i] = 1;
+            while (!playerPassCount.Any(p => p == maxPasses))
+            {
+                if (playerPassCount[i] % 2 == 0)
+                {
+                    i = (((i - shiftLeftOrRightCount) % playersCount) + playersCount) % playersCount;
+                }
+                else
+                {
+                    i = (i + shiftLeftOrRightCount) % playersCount;
+                }
+
+                playerPassCount[i]++;
+                passCount++;
+            }
+
+            return passCount;
+        }
+    }
+
+}
