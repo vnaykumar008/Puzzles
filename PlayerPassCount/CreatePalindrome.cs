3819dbe2ce8955ca39d89e4229d58749b31fe574
Vinay Kumar Nadiminti <vnaykumar008@gmail.com>
2019-07-24 20:58:25 +0530

    Create palindrome with given string and return number of changes taken to make the given string as palindrome.

---
+++ PlayerPassCount/CreatePalindrome.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayerPassCount
+{
+    public class CreatePalindrome
+    {
+        public int PalindromeMakingCount(string input)
+        {
+            List<char> arrayList = input.ToList();
+            int makingCount = 0;
+            for (int startindex = 0, endindex = input.Length - 1; startindex < endindex; startindex++)
+            {
+                if (arrayList[startindex] == arrayList[endindex])
+                {
+                    endindex--;
+                    continue;
+                }
+                else
+                {
+                    arrayList.Insert(endindex + 1, arrayList[startindex]);
+                    makingCount++;
+                }
+            }
+            return makingCount;
+        }
+    }
+}
